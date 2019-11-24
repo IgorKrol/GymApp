4708921a496c5936a993301e5d6cb5d1ff828bd5
Denis <aripash@gmail.com>
2019-11-24 20:57:01 +0100

    Revert "revert"
    This reverts commit 92c3efa48e5e1f7d18c488e2b66dcbe95c8ac31a.

--- app/src/main/java/com/example/gymapp/Main.java
+++ app/src/main/java/com/example/gymapp/Main.java
@@ -3,19 +3,25 @@ package com.example.gymapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Main extends AppCompatActivity {
-
+public class Main extends AppCompatActivity implements View.OnClickListener {
+    Button News;
+    Button Instructors;
+    Button Fitness_Divices;
+    Button Fitness_Advice;
     @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
+            News.setOnClickListener(this);
+            Instructors.setOnClickListener(this);
+            Fitness_Divices.setOnClickListener(this);
+            Fitness_Advice.setOnClickListener(this);
         }
 
-<<<<<<< HEAD
     public void onClick(View view) {
 
     }
-=======
->>>>>>> parent of a31d594... first commit
     }
