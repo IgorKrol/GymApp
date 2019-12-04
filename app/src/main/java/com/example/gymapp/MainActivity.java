04e6d21cd6647fbdf1b4332a724592620f469a85
Denis <aripash@gmail.com>
2019-12-04 20:35:27 +0100

    v0.01

--- app/src/main/java/com/example/gymapp/MainActivity.java
+++ app/src/main/java/com/example/gymapp/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.gymapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnInstructors;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnInstructors = (Button)findViewById(R.id.instractors_button);
+        btnInstructors.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this,instructors.class));
+            }
+        });
     }
+
+
+
 }
