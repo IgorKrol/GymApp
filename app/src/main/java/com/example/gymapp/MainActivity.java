bd9ed046c2f946d704e34396adca83b5aeba5023
IgorKrol <igork08@gmail.com>
2019-12-07 10:20:47 +0200

    Memu Bar with Login new version

--- app/src/main/java/com/example/gymapp/MainActivity.java
+++ app/src/main/java/com/example/gymapp/MainActivity.java
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.*;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button btnInstructors;
-    Button btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,21 +18,31 @@ public class MainActivity extends AppCompatActivity {
 
         btnInstructors = (Button)findViewById(R.id.instractors_button);
         btnInstructors.setOnClickListener(new View.OnClickListener() {
-                                              @Override
-                                              public void onClick(View v) {
-                                                  startActivity(new Intent(MainActivity.this, instructors.class));
-                                              }
-                                          });
-
-        btnLogin = (Button)findViewById(R.id.login_button);
-        btnLogin.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v){
-                    startActivity(new Intent(MainActivity.this,LoginActivity.class));
-                }
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, instructors.class));
+            }
         });
     }
 
+    /* menu creation */
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
-
+    /* menu options */
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.login:
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
+
+
+
